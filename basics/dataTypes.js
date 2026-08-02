0133bb3146c8ed22d776d75605826a9efaf67976
Abhijeet Kumar <166986779+Abhijeet1starts@users.noreply.github.com>
2026-08-02 11:10:48 +0000

    dataTypes done

---
+++ basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3+4)   // we are using nodejs, not browser
+
+// Data types
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object (user defined)
+
+let name = "abhi"
+let age = 20
+let isLoggedIn = true;
+
+console.log(typeof "ABHI")
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof null)
+console.log(typeof undefined)
